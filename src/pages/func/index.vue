5f9b82e8599070953d6c7612e129f1787ca71aaf
Ida-yang <1549048123@qq.com>
2019-02-21 17:39:41 +0800

    first commit

---
+++ src/pages/func/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg:'功能'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
